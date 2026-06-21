e2c1ee98fbd089f6cf0883b4301f989897f9cb9c
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-06-22 00:30:45 +0500

    Practice of phase 1

--- DataTypeAndVariables.js
+++ DataTypeAndVariables.js
@@ -1,15 +1,102 @@
-// Exercise 1
+// // Exercise 1
 
-// Create variables for:
+// // Create variables for:
 
-// Your name
-// Your age
-// Your country
-// Whether you are a student
+// // Your name
+// // Your age
+// // Your country
+// // Whether you are a student
 
-// Print everything.
+// // Print everything.
 
-let name = "Sarim";
-let age = 18;
-let country = "Pakistan"
+// let name = "Sarim";
+// let age = 18;
+// let country = "Pakistan"
+// let isStudent = true;
+
+// console.log(name);
+// console.log(age);
+// console.log(country);
+// console.log(`Student: ${isStudent}`);
+
+// // Exercise 2
+
+// // Store two numbers.
+
+// // Print:
+
+// // Sum
+// // Difference
+// // Product
+// // Quotient
+
+// let val1 = 4;
+// let val2 = 2;
+
+// console.log(`Sum: ${4+2}`);
+// console.log(`Difference: ${4-2}`);
+// console.log(`Product: ${4*2}`);
+// console.log(`Quotient: ${4/2}`);
+
+
+// // Exercise 3
+
+// // Store your birth year.
+
+// // Calculate your age.
+
+// let birthYear = 2007;
+
+// console.log(`Age: ${2025 - birthYear}`);
+
+// // Exercise 4
+
+// // Store the price of three products.
+
+// // Print the total.
+
+// let price1 = 50;
+// let price2 = 250;
+// let price3 = 1000;
+
+// console.log(`Total Price: ${price1 + price2 + price3}`);
+
+
+// // Exercise 5
+
+// // Swap two variables.
+
+// let var1 = 10;
+// let var2 = 20;
+// let tempVar;
+
+// console.log(`Before Swapping: ${var1}, ${var2}`);
+
+// tempVar = var1;
+// var1 = var2;
+// var2 = tempVar;
+
+// console.log(`After Swapping: ${var1}, ${var2}`);
+
+
+// // Exercise 6
+
+// // Ask the user for their name.
+
+// let userName = prompt("Enter You Name: ");
+
+// console.log(`Hello ${userName}`);
+
+
+// // Exercise 7
+
+// // Ask for age.
+
+// // Print
+
+// let userAge= prompt("Enter You Age: ");
+
+// console.log(`You are  ${userAge} years`);
+
+
 
