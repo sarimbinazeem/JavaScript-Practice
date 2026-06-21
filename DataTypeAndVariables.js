4ab77840b7aaadd501bbd2551ebef53fff0cb665
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-06-21 23:52:26 +0500

    Added First Phase Files

---
+++ DataTypeAndVariables.js
@@ -0,0 +1,15 @@
+// Exercise 1
+
+// Create variables for:
+
+// Your name
+// Your age
+// Your country
+// Whether you are a student
+
+// Print everything.
+
+let name = "Sarim";
+let age = 18;
+let country = "Pakistan"
+
