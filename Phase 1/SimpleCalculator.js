89ccb917b7e017a903bf517d1f795289415b897d
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-06-23 14:47:10 +0500

    Phase 1: Simple calcualtor

---
+++ Phase 1/SimpleCalculator.js
@@ -0,0 +1,8 @@
+let val1 = Number(prompt("Enter Value 1: "));
+let val2 = Number(prompt("Enter Value 2: "));
+
+
+console.log(`Sum: ${val1 + val2}`);
+console.log(`Product: ${val1 * val2}`);
+console.log(`Difference: ${val1 - val2}`);
+console.log(`Division: ${val1/val2}`);
