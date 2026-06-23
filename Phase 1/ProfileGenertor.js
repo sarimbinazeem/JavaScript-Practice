c1844d012e180ea2c49a66e5783a322142a1a16c
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-06-23 14:40:34 +0500

    Phase1: Profile Generator

---
+++ Phase 1/ProfileGenertor.js
@@ -0,0 +1,12 @@
+let profile = {
+    name: "Sarim Bin Azeem",
+    age: 18,
+    country: "Pakistan",
+    isStudent: true
+
+}
+
+console.log(`Name: ${profile["name"]}`);
+console.log(`Age: ${profile["age"]}`);
+console.log(`Country: ${profile["country"]}`);
+console.log(`Student: ${profile["isStudent"]}`);
