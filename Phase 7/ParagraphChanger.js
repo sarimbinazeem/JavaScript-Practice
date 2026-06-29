e082e0d685caa7167de838f04b33ec222700a0e2
Sarim Bin Azeem <k250574@nu.edu.pk>
2026-06-29 11:00:10 +0500

    Phhase 7: paragraph changer

---
+++ Phase 7/ParagraphChanger.js
@@ -0,0 +1,7 @@
+let text = document.querySelector(".text");
+let btn = document.querySelector(".btn");
+
+btn.addEventListener("click", () =>
+{
+    text.innerText = "Text Changed Successfully!";
+})
