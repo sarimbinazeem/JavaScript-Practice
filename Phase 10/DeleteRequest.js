b0ec42068b62deb4686e51b33e9c7646e4585456
Sarim Bin Azeem <sarimbinaz@gmail.com>
2026-06-30 07:41:02 -0700

    Phase 10: Delete Request

---
+++ Phase 10/DeleteRequest.js
@@ -0,0 +1,100 @@
+async function createPost()
+{
+    try
+    {
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts",
+            {
+                method:"POST",
+
+                headers:
+                {
+                    "Content-Type":"application/json"
+                },
+
+                body:JSON.stringify(
+                    {
+                        title: "JavaScript",
+                        body: "Learning Fetch API",
+                        userId: 1
+                    }
+                )
+            }
+        );
+
+        let data = await response.json();
+        
+        console.log("====Posted Request====");
+        console.log(data);
+
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+
+async function updatePost()
+{
+    try
+    {
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts/1",
+            {
+                method:"PATCH",
+
+                headers:
+                {
+                    "Content-Type":"application/json"
+                },
+
+                body: JSON.stringify(
+                    {
+                        title: "Advanced Javascript"
+
+                    }
+                )
+            }
+        );
+
+        let data = await response.json();
+
+        console.log("====Patched Request====");
+        console.log(data);
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+async function deletePost()
+{
+    try
+    {
+        let response = await fetch("https://jsonplaceholder.typicode.com/posts/1",
+            {
+                method:"DELETE"
+            }
+        );
+
+        if(response.ok)
+        {
+            console.log("Post Deleted Successfully!");
+        }
+    }
+    catch(error)
+    {
+        console.log(error);
+    }
+}
+
+
+
+async function main()
+{
+    await createPost();
+    await updatePost();
+    await deletePost();
+}
+
+main();
